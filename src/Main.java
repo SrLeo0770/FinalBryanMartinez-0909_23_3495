6430b1964bbc08c318eb6d5ea3d1d85c9e94a75f
SrLeo0770 <bmartinezd3@miumg.edu.gt>
2024-06-15 08:17:10 -0600

    Menu ejercicio 1

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    private static ArrayList<String> estudiantes = new ArrayList<>();
+
+    public static void main(String[] args) {
+        boolean salir = false;
+
+        while (!salir) {
+            System.out.println("\n====================");
+            System.out.println("       Menu");
+            System.out.println("====================");
+            System.out.println("1. Juntar resultados");
+            System.out.println("2. Tabla de multiplicar");
+            System.out.println("3. Calculadora basica");
+            System.out.println("4. Numero primo o no");
+            System.out.println("5. Salir");
+            System.out.println("====================");
+            System.out.print("Ingrese su opcion: ");
+            int opcion = scanner.nextInt();
+            scanner.nextLine();
+
+            }
+        }
+
+
+    }
